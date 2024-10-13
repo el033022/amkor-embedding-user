269b272a8fc7735f5d7a06416f856a9df108b3fd
ecpar <ecpar@trends.com.ph>
2024-10-13 22:17:02 +0800

    update

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-## User Owns Data Embedding
+# Embed for your organization (also known as user owns data)  
+The application allows you to integrate data, reports, dashboards, and tiles that are accessible to users in your organization when they sign in using their own Power BI accounts. In this type of application, the users own the data that you embed.
+
+
 
 ### API Permissions:
 - Add premissions
@@ -7,4 +10,44 @@
   - `Report.Read.All`
   - `Dataset.ReadWrite.All`
   - `App.Read.All`
+
+`carola@tgcloudtest.onmicrosoft.com`  
+`Tujo121743`  
+`October4!~>`  
+![](./public/img/img1.png)
+
+`nicolecruz@tgcloudtest.onmicrosoft.com`
+`Poko311284`
+
+# Considerations and limitations
+- Additional manual inputs to the button aren't supported.
+- The visual isn't supported for **embedded analytics.**
+- The visual doesn't work in Publish to Web (public) scenarios, because unauthenticated scenarios aren't supported by Power Automate.
+- The visual doesn't support export scenarios.
+- The Power Automate visual is limited to process a maximum of 1000 records.
+- The user running the flow within the Power BI report needs to have permissions to run the flow. Grant these permissions through direct sharing by the user or by sharing to a Microsoft Entra group.
+- Create flows that you will use with the Power BI button trigger directly within the Power BI report. Avoid going directly to Power Automate to create these flows, as the - trigger will not have access to the data fields necessary for creating the flow.
+- Power BI data is sent to, and processed in, a geography where the flow is deployed.
+
+
+
+### Comparison Table
+
+| Feature                | Embed for Organization                       | Embed for Customers                        | iFrame                                       |
+| ---------------------- | -------------------------------------------- | ------------------------------------------ | -------------------------------------------- |
+| **Authentication**     | Single Sign-On (SSO)                         | Can use custom sign-in methods             | May require re-login                         |
+| **User Management**    | Managed via Azure AD                         | Custom user management available           | No built-in user management                  |
+| **Security**           | Strong security with Azure AD integration    | Potential security risks if not managed    | Security risks (e.g., clickjacking)          |
+| **Row-Level Security** | Yes, supports row-level security             | Yes, but requires careful setup            | Limited or no support                        |
+| **Customization**      | Limited customization options                | Custom branding and UI adjustments         | Limited customization                        |
+| **Integration**        | Seamless with Microsoft ecosystem            | Can integrate with custom applications     | Limited integration capabilities             |
+| **Setup Complexity**   | Requires Azure AD and Power BI configuration | More complex due to custom user management | Simple and quick setup                       |
+| **Accessibility**      | Internal users only                          | Accessible to external users               | Login using Power BI Account                 |
+| **Pipeline Trigger**   | Can execute workflows with session token     | Requires explicit permissions setup        | Limited capability                           |
+| **Email Sending**      | Can send emails using session token          | Can send emails, but requires user setup   | Not directly supported                       |
+| **User Experience**    | Seamless experience for internal users       | Tailored experience for custom users       | Basic content display; limited interactivity |
+
+
+  
+
  
--- app.js
+++ app.js
@@ -1,9 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const app = express();
 const msal = require('@azure/msal-node');
 const session = require('express-session');
-const helmet = require("helmet");
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -24,7 +24,6 @@ app.use(
 
 // Middleware for logging requests (example of custom middleware)
 app.use((req, res, next) => {
-    console.log('req headers', req.headers);
     console.log(`${req.method} request for '${req.url}'`);
     next();
 });
@@ -32,12 +31,34 @@ app.use((req, res, next) => {
 // MSAL configuration
 const msalConfig = {
     auth: {
-        clientId: '',              // Your Azure AD Application (client) ID
-        authority: 'https://login.microsoftonline.com/TENANT_ID', // Your Azure AD tenant ID
-        clientSecret: '',      // Your Azure AD Application client secret
-        redirectUri: 'http://localhost:3456/redirect',  // Redirect URI (must match Azure AD configuration)
+        clientId: process.env.CLIENT_ID, // Your Azure AD Application client ID
+        authority: process.env.AUTHORITY, // Your Azure AD tenant ID
+        clientSecret: process.env.CLIENT_SECRET, // Your Azure AD Application client
+        redirectUri: "http://localhost:3456/redirect", // Redirect URI (must match Azure AD configuration)
     },
 };
+
+const scopes = [
+    'https://analysis.windows.net/powerbi/api/Report.Read.All',
+    'https://analysis.windows.net/powerbi/api/Dataset.Read.All',
+    'https://analysis.windows.net/powerbi/api/App.Read.All',
+    'https://analysis.windows.net/powerbi/api/Item.Execute.All',
+]
+
+const graphScopes = ["https://graph.microsoft.com/.default"];
+
+async function acquireGraphToken() {
+    const accounts = await cca.getTokenCache().getAllAccounts();
+    console.log(accounts);
+    if (accounts.length > 0) {
+        const silentRequest = {
+            account: accounts[0],
+            scopes: graphScopes,
+        };
+
+        return cca.acquireTokenSilent(silentRequest);
+    }
+}
 
 // Create MSAL application object
 const cca = new msal.ConfidentialClientApplication(msalConfig);
@@ -54,7 +75,7 @@ app.get('/', (req, res) => {
 // Login route (initiates sign-in)
 app.get('/login', (req, res) => {
     const authUrl = cca.getAuthCodeUrl({
-        scopes: ['https://analysis.windows.net/powerbi/api/.default'],
+        scopes,
         redirectUri: msalConfig.auth.redirectUri,
     });
 
@@ -71,7 +92,7 @@ app.get('/login', (req, res) => {
 app.get('/redirect', (req, res) => {
     const tokenRequest = {
         code: req.query.code,
-        scopes: ['https://analysis.windows.net/powerbi/api/.default'],
+        scopes,
         redirectUri: msalConfig.auth.redirectUri,
     };
 
@@ -79,6 +100,7 @@ app.get('/redirect', (req, res) => {
         .then((response) => {
             // console.log('\nResponse: \n:', response);
             req.session.user = response;
+            console.log('token:', response.accessToken);
             res.redirect('/dashboard');
         })
         .catch((error) => {
@@ -146,14 +168,181 @@ app.get('/embed-token', async (req, res) => {
 
 });
 
+app.get('/trigger-pipeline', async (req, res) => {
+
+    const URL = "https://api.fabric.microsoft.com/v1/workspaces/6be6316e-d8e9-4751-84c3-34c6eebab80f/items/b8d9d0a3-ee71-4f86-96dd-2579f3ee4b16/jobs/instances?jobType=Pipeline";
+
+    fetch(URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${req.session.user.accessToken}`
+        }
+    })
+        .then(response => console.log(response))
+        .catch(error => console.error('Error triggering pipeline:', error))
+        .finally(() => res.sendStatus(200));
+
+    //itemid b8d9d0a3-ee71-4f86-96dd-2579f3ee4b16
+    //workspaceid 6be6316e-d8e9-4751-84c3-34c6eebab80f
+    //type Pipeline 
+});
+
+app.get('/send-email', async (req, res) => {
+
+    try {
+
+        const URL = "https://api.powerbi.com/v1.0/myorg/reports/1c0eaa4e-059d-434c-a1de-c1a32bbd66a0/ExportTo";
+
+        const resp = await fetch(URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${req.session.user.accessToken}`
+            },
+            body: JSON.stringify({
+                "format": "PNG",
+                "powerBIReportConfiguration": {
+                    "pages": [
+                        {
+                            "pageName": "ReportSection",  // Replace with your specific page name
+                            "visualName": "3a1aeaede6fc79fe5066"  // Replace with your specific visual name
+                        }
+                    ]
+                }
+            })
+        })
+
+        const { id } = await resp.json();
+
+        const pollInterval = setInterval(async () => {
+
+            const exportStatus = await pollExportStatus(id, req.session.user.accessToken);
+
+            if (exportStatus.status === 'Succeeded') {
+                clearInterval(pollInterval);
+                console.log('Export status:', exportStatus);
+                console.log('Export succeeded');
+                const b64 = await downloadAndConvertToBase64(exportStatus, req.session.user.accessToken);
+                if (!b64) res.sendStatus(500);
+                const emailResponse = await sendEmail(req.session.user.accessToken, b64);
+                console.log('Email response:', emailResponse);
+                res.send('<img src="data:image/png;base64,' + b64 + '" />');
+                // res.sendStatus(200);
+
+            }
+
+        }, 5000);
+
+
+    } catch (error) {
+        console.error('Error sending email:', error);
+        res.sendStatus(500);
+    }
+
+});
+
+async function sendEmail(token, b64) {
+
+    try {
+
+        const { accessToken } = await acquireGraphToken();
+
+        const URL = "https://graph.microsoft.com/v1.0/me/sendMail";
+
+        const response = await fetch(URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${accessToken}`
+            },
+            body: JSON.stringify({
+                "message": {
+                    "subject": "Power BI Report",
+                    "body": {
+                        "contentType": "HTML",
+                        "content": "<p>Power BI Report</p>"
+                    },
+                    "toRecipients": [
+                        {
+                            "emailAddress": {
+                                "address": "ecpar@trends.com.ph",
+                            }
+                        }
+                    ],
+                    "attachments": [
+                        {
+                            "@odata.type": "#microsoft.graph.fileAttachment",
+                            "name": "report.png",
+                            "contentBytes": b64
+                        }
+                    ]
+                }
+            })
+        });
+
+        return await response.json();
+
+    } catch (error) {
+        console.error('Error sending email:', error);
+        return null;
+
+    }
+}
+
+async function downloadAndConvertToBase64(status, token) {
+
+    // https://api.powerbi.com/v1.0/myorg/reports/1c0eaa4e-059d-434c-a1de-c1a32bbd66a0/exports/MS9...ZjPS4=/file
+    const URL = status.resourceLocation;
+
+    const response = await fetch(URL, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        }
+    });
+
+    if (!response.ok) {
+        return null;
+    }
+
+    const arrayBuffer = await response.arrayBuffer();
+
+    const base64Data = Buffer.from(arrayBuffer).toString('base64');
+
+    return base64Data;
+
+}
+
+
+async function pollExportStatus(id, token) {
+
+    console.log('Exporting image...');
+
+    const pollUrl = `https://api.powerbi.com/v1.0/myorg/reports/1c0eaa4e-059d-434c-a1de-c1a32bbd66a0/exports/${id}`
+
+    const pollRes = await fetch(pollUrl, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        }
+    });
+
+    return await pollRes.json();
+
+}
+
 // Dashboard route
 app.get('/dashboard', (req, res) => {
-    console.log(req.session);
+    // console.log(req.session);
     if (req.session.user) {
         res.render('dashboard', {
             username: req.session.user.account.username,
             name: req.session.user.name,
         });
+        // res.redirect('https://app.powerbi.com/reportEmbed?reportId=1c0eaa4e-059d-434c-a1de-c1a32bbd66a0&autoAuth=true&ctid=b9d2e4d7-3331-44aa-b735-189229b4c840');
     } else {
         res.redirect('/');
     }
